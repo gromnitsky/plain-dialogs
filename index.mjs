8b328c26ee161486a405bc38a767426f313a1c8e
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2017-12-04 23:42:49 +0200

    combine small common parts into a factory

--- index.mjs
+++ index.mjs
@@ -1,41 +1,41 @@
 /* globals dialogPolyfill */
 
 export function alert(msg, opt) {
-    opt = opt_parse(opt)
-    let node = dialog()
-    node.innerHTML = html(`
-<div class='${css("body")}'><p>${opt.escape ? escape(msg) : msg}</p></div>
-<div class='${css("footer")}'><button>OK</button></div>`, opt)
+    let d = Dialog(opt)
+    d.node.innerHTML = d.html(`
+<div class='${d.css("body")}'><p>${d.opt.escape ? escape(msg) : msg}</p></div>
+<div class='${d.css("footer")}'><button>OK</button></div>`)
 
     return new Promise( (resolve, _) => {
-	node.querySelector('button').onclick = () => node.close()
-	node.onclose = evt => {
+	let btn = d.node.querySelector('button')
+	btn.onclick = () => d.node.close()
+	d.node.onclose = evt => {
 	    document.body.removeChild(evt.target)
 	    resolve(true)
 	}
 
-	show(node, opt)
-	node.querySelector('button').focus()
+	d.show()
+	btn.focus()
     })
 }
 
 function _confirm(msg, opt) {
-    opt = opt_parse(opt)
-    let node = dialog()
-    node.innerHTML = html(`
-<div class='${css("body")}'><p>${opt.escape ? escape(msg) : msg}</p></div>
-<div class='${css("footer")}'>
-  <button class='${css("btn--cancel")}'>Cancel</button>
-  <button class='${css("btn--ok")}'>OK</button>
-</div>`, opt)
+    let d = Dialog(opt)
+    d.node.innerHTML = d.html(`
+<div class='${d.css("body")}'><p>${d.opt.escape ? escape(msg) : msg}</p></div>
+<div class='${d.css("footer")}'>
+  <button class='${d.css("btn--cancel")}'>Cancel</button>
+  <button class='${d.css("btn--ok")}'>OK</button>
+</div>`)
 
     return new Promise( (resolve, reject) => {
-	let btn_cancel = node.querySelector('.'+css("btn--cancel"))
-	btn_cancel.onclick = () => node.close()
-	node.querySelector('.'+css("btn--ok")).onclick = () => node.close('ok')
-	node.onclose = (evt) => {
+	let btn_cancel = d.node.querySelector('.'+d.css("btn--cancel"))
+	btn_cancel.onclick = () => d.node.close()
+	d.node.querySelector('.'+d.css("btn--ok"))
+	    .onclick = () => d.node.close('ok')
+	d.node.onclose = (evt) => {
 	    document.body.removeChild(evt.target)
-	    if (opt._promise_always_resolves) {
+	    if (d.opt._promise_always_resolves) {
 		resolve(evt.target.returnValue === 'ok')
 	    } else {
 		evt.target.returnValue === 'ok' ? resolve(true) : reject(false)
@@ -43,7 +43,7 @@ function _confirm(msg, opt) {
 	}
 
 	btn_cancel.focus()
-	show(node, opt)
+	d.show()
     })
 }
 
@@ -57,13 +57,38 @@ export function confirm2(msg, opt) {
     return _confirm(msg, opt)
 }
 
-function opt_parse(opt) {
-    let def = {
-	escape: true,
-	modal: true,
+// Crockford-style 'say no to this'
+function Dialog(opt) {
+    let opt_parse = opt => {
+	let def = {
+	    escape: true,
+	    modal: true,
+	}
+	if (typeof opt === 'object') return Object.assign(def, opt)
+	return def
     }
-    if (typeof opt === 'object') return Object.assign(def, opt)
-    return def
+
+    let css = t => 'b59LdZ-dlg' + (t ? `__${t}` : '')
+
+    opt = opt_parse(opt)
+    let node = document.createElement('dialog')
+    node.className = css()
+    if (typeof dialogPolyfill !== 'undefined')
+	dialogPolyfill.registerDialog(node)
+
+    return {
+	opt,
+	node,
+	css,
+	html: t => {
+	    let head = opt.title ? `<div class='${css("header")}'>${escape(opt.title)}</div>` : ''
+	    return head + t
+	},
+	show: () => {
+	    document.body.appendChild(node)
+	    node[opt.modal ? 'showModal' : 'show']()
+	}
+    }
 }
 
 function escape(t) {
@@ -75,24 +100,4 @@ function escape(t) {
 	"'": '&#39;',
 	'`': '&#x60;',
     }[char])).replace(/\n/g, '<br>')
-}
-
-function css(t) { return 'b59LdZ-dlg' + (t ? `__${t}` : '') }
-
-function dialog() {
-    let node = document.createElement('dialog')
-    node.className = css()
-    if (typeof dialogPolyfill !== 'undefined')
-	dialogPolyfill.registerDialog(node)
-    return node
-}
-
-function html(t, opt) {
-    let head = opt.title ? `<div class='${css("header")}'>${escape(opt.title)}</div>` : ''
-    return head + t
-}
-
-function show(node, opt) {
-    document.body.appendChild(node)
-    node[opt.modal ? 'showModal' : 'show']()
 }
